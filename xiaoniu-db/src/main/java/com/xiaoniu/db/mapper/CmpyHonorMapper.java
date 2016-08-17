70da6d8ce173d6b564aabe927da77e01d5870cb6
zengxianxian <474183700@qq.com>
2016-08-17 22:44:58 +0800

    update

---
+++ xiaoniu-db/src/main/java/com/xiaoniu/db/mapper/CmpyHonorMapper.java
@@ -0,0 +1,7 @@
+package com.xiaoniu.db.mapper;
+
+import com.xiaoniu.db.domain.CmpyHonor;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CmpyHonorMapper extends Mapper<CmpyHonor> {
+}
---
+++ xiaoniu-db/src/main/java/com/xiaoniu/db/mapper/CmpyPageIntroductionMapper.java
@@ -0,0 +1,7 @@
+package com.xiaoniu.db.mapper;
+
+import com.xiaoniu.db.domain.CmpyPageIntroduction;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CmpyPageIntroductionMapper extends Mapper<CmpyPageIntroduction> {
+}
